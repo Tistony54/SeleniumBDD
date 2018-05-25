1109d279c58c67b4c20d18d1bedbc6a29b2ee3d3
tistony.ashish <tistony.ashish@ASHISH-DUDE>
2018-05-25 15:37:58 +0530

    initial commit to master branch

---
+++ com.seleniumBDD/src/main/java/com/seleniumBDD/com/seleniumBDD/App.java
@@ -0,0 +1,13 @@
+package com.seleniumBDD.com.seleniumBDD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
